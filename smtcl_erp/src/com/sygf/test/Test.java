b5e45ea7fc88889c13950ab19b822d5fea606158
qianfenglai <qianfenglai@i5cnc.com>
2017-02-17 06:16:27 +0800

    alai

--- smtcl_erp/src/com/sygf/test/Test.java
+++ smtcl_erp/src/com/sygf/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void  main(String[] ss){
 		for(int i=0;i<15;i++){
 			try{
-				System.out.println(PLC.getIntance().line.getCarry(i).id);
+				System.out.println(PLC.getIntance().line.getCarry(i).getÔØ¾ßÐòºÅ());
 			}catch(Exception e){
 				System.out.println(i+"=null");
 			}
