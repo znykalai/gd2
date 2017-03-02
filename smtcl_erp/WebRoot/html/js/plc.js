af916b5469e230ee0d539cfa4da1b55282a0873b
guofeng <GuoAdmini5cnc>
2017-03-02 19:17:16 +0800

    plc 全部完成！

--- smtcl_erp/WebRoot/html/js/plc.js
+++ smtcl_erp/WebRoot/html/js/plc.js
@@ -13,55 +13,38 @@ var readyShow = {
 			    item.addClass('select');
 			    $(".plc_show").children().css("display", "none");
 			    $("#plc_show" + $(item).attr('data')).css("display", "block");
-			    if($(item).attr('data')==1){
+			    var gw = null;
+			    //如果当前是装配区A 不需要重复调用刷新事件
+			    if($(item).attr('data')==1&&af.showType=="A"){
+			    	return item=null;
+			    //如果当前是装配区B 不需要重复调用刷新事件
+			    }else if($(item).attr('data')==2&&af.showType=="B"){
+			    	return item=null;
+			    //如果当前是返回PLC工位A 则不需要重复调用刷新事件
+			    }else if($(item).attr('data')==3&&af.showType=="C"){
+			    	return item=null;
+			    //如果当前是返回PLC工位B 则不需要重复调用刷新事件
+			    }else if($(item).attr('data')==4&&af.showType=="D"){
+			    	return item=null;
+			    }else if($(item).attr('data')==1){
 			    	af.showType="A";	//渲染A区
-			    }else{
+			    }else if($(item).attr('data')==2){
 			    	af.showType="B";	//渲染B区
-			    }
-				var a = af.getTableAGW(function(){
+			    }else if($(item).attr('data')==3){
+			    	af.showType="C";	//渲染C区，PLC选择返回工位
+					gw = $("#returnPlcA").val();
+			    }else{
+			    	af.showType="D";	//渲染D区，PLC选择返回工位
+					gw = $("#returnPlcB").val();
+			    };
+				var a = af.getTableGW(function(){//调用渲染html函数
 					return null;
-				},af.showType);
-				return item = null,a=null,type=null;
+				},af.showType,gw),a=null,gw=null;
+				return item=null;
 			});
 			var af = {
 				//默认渲染A区条件
 				showType:"A",
-				//获取所有工位信息
-				action:function(fun,type){
-					var b;
-					if(type=="A"){
-						b=$("table[name='GW'] tbody tr").remove();
-					}else{
-						b=$("table[name='GW_B'] tbody tr").remove();
-					};
-					var a = $.ajax({
-						url: getRootPath()+'/PLCAction.do?operType=getGw',
-						type: 'get',
-						cache:false,
-						data:"type="+type,
-						success: function (data) {
-							var obj = eval("("+data+")");
-							return fun(obj),obj=null;
-						}
-					});
-					return a=null,b=null;
-				},
-				//更新数据函数
-				updateAction:function(map,fun){
-					var a = $.ajax({
-						url: getRootPath()+'/PLCAction.do?operType=updateGw',
-						type: 'get',
-						data: map,
-						cache:false,
-						success: function (data) {
-							if(data.indexOf("成功")==-1){
-								layer.msg("数据已同步，请重新设置");
-							}
-							return fun();
-						}
-					});
-					return a=null;
-				},
 				//物料查询窗体
 				getWuliao:function(fun){
 					try{
@@ -131,35 +114,57 @@ var readyShow = {
 					}
 					return null;
 				},
+				//获取所有工位信息
+				action:function(fun,type,gw){
+					var b;
+					if(type=="A"){
+						b=$("table[name='GW'] tbody tr").remove();
+					}else{
+						b=$("table[name='GW_B'] tbody tr").remove();
+					};
+					var a = $.ajax({
+						url: getRootPath()+'/PLCAction.do?operType=getGw',
+						type: 'get',
+						cache:false,
+						data:"type="+type+"&gw="+gw,
+						success: function (data) {
+							var obj = eval("("+data+")");
+							return fun(obj),obj=null;
+						}
+					});
+					return a=null,b=null;
+				},
+				//更新数据函数
+				updateAction:function(map,fun){
+					var a = $.ajax({
+						url: getRootPath()+'/PLCAction.do?operType=updateGw',
+						type: 'get',
+						data: map,
+						cache:false,
+						success: function (data) {
+							if(data.indexOf("成功")==-1){
+								layer.msg("数据已同步，请重新设置");
+							}
+							return fun();
+						}
+					});
+					return a=null;
+				},
 				//获取当前工位所修改的值并且提交;
 				setValue:function(e,map){
-					var row = $(e).attr("id").split("_");
 					var data = {
 						cm:map.cm,
 						gwType:map.gwType,
 						gw:$(e).parent().attr("GW"),
-						name:$('#'+map.name+''+row[1]+'_'+row[2]).attr("title"),
+						name:$('#'+map.name).attr("title"),
 						value:$(e).attr("title"),
 						oldValue:$(e).attr('oldValue')
 					};
 					var b=af.updateAction(data,function(){
-						var c = af.action(function(e){
-							var mm = {tableId:"A_GW",trId:"newTr_A",
-								tdId1:"newTd1_",tdId2:"newTd2_",
-								tdId3:"newTd3_",gwType:"A",BOOL_DIZHI:'_A'
-							};
-							if(af.showType=="B"){
-								mm = {
-									tableId:"B_GW",trId:"newTr_B",
-									tdId1:"BnewTd1_",tdId2:"BnewTd2_",
-									tdId3:"BnewTd3_",gwType:"B",BOOL_DIZHI:'_B',
-								};
-							};
-							af.showHtml(e,mm),mm=null;
-						},af.showType);
-						return c=null;
+						var a=$("#shuxin").click();//刷新数据
+						return a=null;
 					});
-					return b=null,data=null,row=null,map=null,e=null;
+					return b=null,map=null,e=null,data=null;
 				},
 				//双击事件
 				dblclick:function(e,map){
@@ -185,6 +190,10 @@ var readyShow = {
 								});
 							//只允许输入数字
 							}else{
+								//如果是machineID 不做任何改变
+								if($('#'+map.name).attr('title')=="machineID"){
+									return null;
+								};
 								$(e).html('<input id="td_input" style="padding:0;font-size:10px;width:100%;height:19px;" type="number" min="0" class="form-control" value="' + $(e).attr('title') + '">');
 								$("#td_input").focus();
 								$("#td_input").blur(function (){
@@ -222,8 +231,12 @@ var readyShow = {
 						$("div[name='ssx']").attr("class","plc_gongwei_hui");
 						while(j<obj.A_SSX.length){
 							try{
-								$("#ssx"+j).html(obj.A_SSX[j][j]);
-								$("#ssx"+j).attr("class","plc_gongwei_lan");
+								if(obj.A_SSX[j][j]&&obj.A_SSX[j][j]!=""){
+									$("#ssx"+j).html(obj.A_SSX[j][j]);
+									$("#ssx"+j).attr("class","plc_gongwei_lan");
+								}else{
+									$("#ssx"+j).html("");
+								}
 							}catch (e) {
 								alert(e);
 							}
@@ -236,86 +249,190 @@ var readyShow = {
 					}
 					return null;
 				},
-				//A区需要被渲染的html
+				//A区需要被渲染的html,obj=后台返回数据，mm=当前的装配区以及各个元素的ID
 				showHtml:function(obj,mm){
-					var j=0;
-					while(j<obj.data.length){
-						var i=0;
-						var map = obj.data[j];
-						while(i<map.A_LIST.length){
-							if(map.A_LIST[i].A.indexOf('-')==-1&&map.A_LIST[i].A!="boolContent"){
-								$('#'+mm.tableId+''+j+' tbody').append('<tr name="'+mm.trId+'" GW="'+j+'" bgcolor="#ffffff" style="height:20px;">' +
-									//项目
-									'<td id="'+mm.tdId1+''+j+'_'+i+'" title="'+map.A_LIST[i].A+'" style="width:100px;font-size:12px;">' +
-										(map.A_LIST[i].A.length>6?map.A_LIST[i].A.substring(0,7)+'..':map.A_LIST[i].A) +
-									'</td>' +
-									//列队1
-									'<td id="'+mm.tdId2+''+j+'_'+i+'" title="'+map.A_LIST[i].B+'" oldValue="'+map.A_LIST[i].B+'" style="width:50px;font-size:12px;">' +
-										(map.A_LIST[i].B.length>6?map.A_LIST[i].B.substring(0,6)+'..':map.A_LIST[i].B) +
-									'</td>' +
-									//列队2
-									'<td id="'+mm.tdId3+''+j+'_'+i+'" title="'+map.A_LIST[i].C+'" oldValue="'+map.A_LIST[i].C+'"  style="width:50px;font-size:12px;">'+map.A_LIST[i].C+'</td>' +
-								'</tr>');
-								//队列1
-								$('#'+mm.tdId2+''+j+'_'+i).dblclick(function(){
-									var map = {
-										cm:"firstST",
-										name:mm.tdId1,
-										gwType:mm.gwType
-									};
-									var a=af.dblclick(this,map);
-									return map=null,a=null;
-								});
-								//队列2
-								$('#'+mm.tdId3+''+j+'_'+i).dblclick(function(){
-									var map = {
-										cm:"secondST",
-										name:mm.tdId1,
-										gwType:mm.gwType
-									};
-									var a=af.dblclick(this,map);
-									return map=null,a=null;
-								});
+					if(mm.type=="A"||mm.type=="B"){
+						var j=0;
+						while(j<obj.data.length){
+							var i=0;
+							var map = obj.data[j];
+							while(i<map.A_LIST.length){
+								if(map.A_LIST[i].A.indexOf('-')==-1&&map.A_LIST[i].A!="boolContent"){
+									$('#'+mm.tableId+''+j+' tbody').append('<tr name="'+mm.trId+'" GW="'+j+'" bgcolor="#ffffff" style="height:20px;">' +
+										//项目
+										'<td id="'+mm.tdId1+''+j+'_'+i+'" title="'+map.A_LIST[i].A+'" style="width:100px;font-size:12px;">' +
+											(map.A_LIST[i].A.length>6?map.A_LIST[i].A.substring(0,7)+'..':map.A_LIST[i].A) +
+										'</td>' +
+										//列队1
+										'<td id="'+mm.tdId2+''+j+'_'+i+'" title="'+map.A_LIST[i].B+'" oldValue="'+map.A_LIST[i].B+'" style="width:50px;font-size:12px;">' +
+											(map.A_LIST[i].B.toString().length>6?map.A_LIST[i].B.toString().substring(0,6)+'..':map.A_LIST[i].B) +
+										'</td>' +
+										//列队2
+										'<td id="'+mm.tdId3+''+j+'_'+i+'" title="'+map.A_LIST[i].C+'" oldValue="'+map.A_LIST[i].C+'"  style="width:50px;font-size:12px;">' +
+											(map.A_LIST[i].C.toString().length>6?map.A_LIST[i].C.toString().substring(0,6)+'..':map.A_LIST[i].C) +
+										'</td>' +
+									'</tr>');
+									//队列1
+									$('#'+mm.tdId2+''+j+'_'+i).dblclick(function(){
+										var row = $(this).attr("id").split("_");
+										var map = {
+											cm:"firstST",
+											name:mm.tdId1+''+row[1]+'_'+row[2],
+											gwType:mm.gwType
+										};
+										var a=af.dblclick(this,map);
+										return map=null,a=null,row=null;
+									});
+									//队列2
+									$('#'+mm.tdId3+''+j+'_'+i).dblclick(function(){
+										var row = $(this).attr("id").split("_");
+										var map = {
+											cm:"secondST",
+											name:mm.tdId1+''+row[1]+'_'+row[2],
+											gwType:mm.gwType
+										};
+										var a=af.dblclick(this,map);
+										return map=null,a=null,row=null;
+									});
+								}
+								i++;
 							}
-							i++;
+							$("#BOOL"+j+"_"+mm.type).val(map.A_BOLL);
+							$("#DIZHI"+j+"_"+mm.type).val(map.A_SDZ);
+							j++,i=null,map=null;
 						}
-						$("#BOOL"+j+""+mm.BOOL_DIZHI).val(map.A_BOLL);
-						$("#DIZHI"+j+""+mm.BOOL_DIZHI).val(map.A_SDZ);
-						j++,i=null,map=null;
-					}
-					//A区输送线
-					af.ssxUpdate(obj);
-					return j=null;
+						//A区输送线&&B区输送线
+						af.ssxUpdate(obj);
+						return j=null;
+					}else{
+						//当前标签不在AB区时，清空AB区的数据，
+						var a=$("table[name='GW'] tbody tr").remove(),a=null;
+						var b=$("table[name='GW_B'] tbody tr").remove(),b=null;
+						var c=$("table[name='returnTablePlc'] tbody tr").remove(),c=null;
+						var map = obj.data[0];
+						var i = 0;
+						while(i<map.A_LIST.length){
+							$('#'+mm.tableId+' tbody').append('<tr name="'+mm.trId+'" GW="'+mm.gw+'" bgcolor="#ffffff" style="height:20px;">' +
+								//项目
+								'<td id="'+mm.tdId1+'_'+i+'" title="'+map.A_LIST[i].A+'" style="width:100px;font-size:12px;">' +
+									(map.A_LIST[i].A.length>15?map.A_LIST[i].A.substring(0,14)+'..':map.A_LIST[i].A) +
+								'</td>' +
+								//列队1
+								'<td id="'+mm.tdId2+'_'+i+'" title="'+map.A_LIST[i].B+'" oldValue="'+map.A_LIST[i].B+'" style="width:50px;font-size:12px;">' +
+									(map.A_LIST[i].B.toString().length>8?map.A_LIST[i].B.substring(0,8)+'..':map.A_LIST[i].B) +
+								'</td>' +
+								//列队2
+								'<td id="'+mm.tdId3+'_'+i+'" title="'+map.A_LIST[i].C+'" oldValue="'+map.A_LIST[i].C+'"  style="width:50px;font-size:12px;">' +
+									(map.A_LIST[i].C.toString().length>8?map.A_LIST[i].C.substring(0,8)+'..':map.A_LIST[i].C) +
+								'</td>' +
+							'</tr>');
+							//队列1
+							$('#'+mm.tdId2+'_'+i).dblclick(function(){
+								var row = $(this).attr("id").split("_");
+								var map = {
+									cm:'',
+									name:mm.tdId1+'_'+row[1],
+									gwType:mm.gwType
+								};
+								var a=af.dblclick(this,map);
+								return map=null,a=null,row=null;
+							});
+							//队列2
+							$('#'+mm.tdId3+'_'+i).dblclick(function(){
+								var row = $(this).attr("id").split("_");
+								var map = {
+									cm:'',
+									name:mm.tdId1+'_'+row[1],
+									gwType:mm.gwType
+								};
+								var a=af.dblclick(this,map);
+								return map=null,a=null,row=null;
+							});
+							i++;
+						};
+						$("#BOOL0_"+mm.type).val(map.A_BOLL);
+						$("#DIZHI0_"+mm.type).val(map.A_SDZ);
+						//A区输送线&&B区输送线
+						af.ssxUpdate(obj);
+						return null;
+					};
+					return null;
 				},
-				//获取所有工位信息启动函数
-				getTableAGW:function(fun,type){
-					var a = this.action(function(obj){
-						var map = {
+				//所以元素ID结合map
+				map:function(type,gw){
+					var map = {};
+					if(type=="A"){//装配区A
+						map = {
+							type:"A",gw:null,
 							tableId:"A_GW",trId:"newTr_A",
 							tdId1:"newTd1_",tdId2:"newTd2_",
-							tdId3:"newTd3_",gwType:"A",BOOL_DIZHI:'_A',
+							tdId3:"newTd3_",gwType:"A"
 						};
-						if(type=="B"){
-							map = {
-								tableId:"B_GW",trId:"newTr_B",
-								tdId1:"BnewTd1_",tdId2:"BnewTd2_",
-								tdId3:"BnewTd3_",gwType:"B",BOOL_DIZHI:'_B',
-							};
-						}
-						af.showHtml(obj,map),map=null,type=null;
-					},type);
-					return fun(),a=null;
+					}else if(type=="B"){//装配区B
+						map = {
+							type:"B",gw:null,
+							tableId:"B_GW",trId:"newTr_B",
+							tdId1:"BnewTd1_",tdId2:"BnewTd2_",
+							tdId3:"BnewTd3_",gwType:"B"
+						};
+					}else if(type=="C"){//PLC选择返回工位A
+						map={
+							gw:gw,
+							type:"C",
+							tableId:"returnTablePlcA",
+							trId:"returnPlcTrA",
+							tdId1:"returnPlcTd1A",tdId2:"returnPlcTd2A",
+							tdId3:"returnPlcTd3A",gwType:"C"
+						};
+					}else if(type=="D"){//PLC选择返回工位B
+						map={
+							gw:gw,
+							type:"D",
+							tableId:"returnTablePlcB",
+							trId:"returnPlcTrB",
+							tdId1:"returnPlcTd1B",tdId2:"returnPlcTd2B",
+							tdId3:"returnPlcTd3B",gwType:"D"
+						};
+					};
+					return map;
+				},
+				//获取所有工位信息启动函数 fun=回调函数，type=当前那个装配区,gw=当前第几个工位
+				getTableGW:function(fun,type,gw){
+					var a = this.action(function(obj){
+						var map = af.map(type,gw);
+						var b=af.showHtml(obj,map);
+						b=null,map=null,type=null,gw=null;
+					},type,gw),a=null;
+					return fun();
 				},
 				//渲染界面函数
 				load:function(fun){
 					//刷新按钮事件绑定
 					$("#shuxin").click(function(){
 						var win = layer.open({type:3});
-						var a = af.getTableAGW(function(){
-							return layer.close(win),win=null;
-						},af.showType);//默认渲染A区
+						var gw = null;
+						if(af.showType=='C'){
+							//光大返回A
+							gw = $("#returnPlcA").val();
+						}else if(af.showType=='D'){
+							//光大返回B
+							gw = $("#returnPlcB").val();
+						};
+						var a = af.getTableGW(function(){
+							return layer.close(win);win=null;
+						},af.showType,gw),gw=null;//默认渲染A区
+						return null;
 					});
 					$("#shuxin").click();
+					//选择PLC返回工位
+					$(".selectpicker").change(function(){
+						var win = layer.open({type:3});
+						var gw = $(this).val();
+						var a = af.getTableGW(function(){
+							return layer.close(win),win=null;
+						},af.showType,gw),gw=null;
+						return null;
+					});
 					return fun();
 				}
 			};
--- smtcl_erp/src/com/sygf/erp/baseData/action/PLCAction.java
+++ smtcl_erp/src/com/sygf/erp/baseData/action/PLCAction.java
@@ -53,68 +53,97 @@ public class PLCAction extends Action{
 			request.setCharacterEncoding("utf-8");
 			response.setCharacterEncoding("utf-8");
 			HashMap typeMap = GetParam.GetParamValue(request, "iso-8859-1", "utf-8");
-//			System.out.println("typeMap="+typeMap);
+			System.out.println("typeMap="+typeMap);
 			JSONObject result = new JSONObject();
-			ArrayList mapList = new ArrayList();
+			ArrayList mapList = new ArrayList();//返回table数据
+			ArrayList sszList = new ArrayList();//返回到输送线
+			//getDataFromGD
 			//A区工位
 			Vector<STContent> A = PLC.getIntance().STC1;
 			//A区输送线
 			CarryLine ss = PLC.getIntance().line;
-			if(typeMap.get("type").equals("B")){
+			if(typeMap.get("type").equals("B")||typeMap.get("type").equals("D")){
 				A = PLC.getIntance().STC2;		//B区工位
 				ss = PLC.getIntance().line2;	//B区输送线
 			};
-			int j = 0;
-			while(j < 16){
-				try{
-					ArrayList list = new ArrayList();
-					HashMap mapA=new HashMap();
-					ST_Father duilie1 = A.get(j).firstST;
-					ST_Father duilie2 = A.get(j).secondST;
-					Vector<HashMap> v=new Vector<HashMap>();
-					Hashtable temp=duilie1.getMap();
-					Hashtable temp2=duilie2.getMap();
-					Enumeration keys=temp.keys();
-					while(keys.hasMoreElements()){
-						HashMap map = new HashMap();
-						Object k=keys.nextElement();
-						Object val=temp.get(k);
-						Object val2=temp2.get(k);
-						if(val instanceof Boolean){
-							map.put("A", k);
-							map.put("B",val );
-							map.put("C",val2 );
-							v.insertElementAt(map, 0);
-						}else{
-							map.put("A", k);
-							map.put("B",val );
-							map.put("C",val2 );
-							v.add(map);	
+			//装配区A，装配区B
+			if(typeMap.get("type").equals("A")||typeMap.get("type").equals("B")){
+				int j = 0;
+				while(j < 16){
+					try{
+						ArrayList list = new ArrayList();
+						HashMap mapA=new HashMap();
+						ST_Father duilie1 = A.get(j).firstST;
+						ST_Father duilie2 = A.get(j).secondST;
+						Vector<HashMap> v=new Vector<HashMap>();
+						Hashtable temp=duilie1.getMap();
+						Hashtable temp2=duilie2.getMap();
+						Enumeration keys=temp.keys();
+						while(keys.hasMoreElements()){
+							HashMap map = new HashMap();
+							Object k=keys.nextElement();
+							Object val=temp.get(k);
+							Object val2=temp2.get(k);
+							if(val instanceof Boolean){
+								map.put("A", k);
+								map.put("B",val );
+								map.put("C",val2 );
+								v.insertElementAt(map, 0);
+							}else{
+								map.put("A", k);
+								map.put("B",val );
+								map.put("C",val2 );
+								v.add(map);	
+							}
 						}
-					}
-					for(int i=0;i<v.size();i++){
-						list.add(v.get(i));
-					}
-					temp=null; temp2=null;keys=null;
-					mapA.put("A_LIST", list);
-					mapA.put("A_BOLL", duilie1.getBoolContent());
-					mapA.put("A_SDZ", duilie1.getStartAddress());
-					mapList.add(mapA);
-					j++;
-				}catch (Exception e) {}
-			}
-			ArrayList sszList = new ArrayList();
+						for(int i=0;i<v.size();i++){
+							list.add(v.get(i));
+						}
+						temp=null; temp2=null;keys=null;
+						mapA.put("A_LIST", list);
+						mapA.put("A_BOLL", duilie1.getBoolContent());
+						mapA.put("A_SDZ", duilie1.getStartAddress());
+						mapList.add(mapA);
+						j++;
+					}catch(Exception e){}
+				};
+			//选择PLC返回工位
+			}else{
+				int line=typeMap.get("type").equals("C")?1:2;
+				int ST=Integer.parseInt(typeMap.get("gw").toString());
+				Hashtable temp = PLC.getIntance().getDataFromGD(ST,line);
+				ArrayList list = new ArrayList();
+				HashMap mapA=new HashMap();
+				Enumeration keys=temp.keys();
+				while(keys.hasMoreElements()){
+					Object k=keys.nextElement();
+					if(k.toString().indexOf("2")==-1&&!(k=="boolCont"||k=="startAddres")){
+						HashMap map = new HashMap();
+						map.put("A", k);
+						map.put("B", temp.get(k));
+						map.put("C", temp.get(k+"2"));
+						list.add(map);
+					};
+				};
+				mapA.put("A_LIST", list);
+				mapA.put("A_BOLL", temp.get("boolCont"));
+				mapA.put("A_SDZ", temp.get("startAddres"));
+				mapList.add(mapA);mapA=null;temp=null;keys=null;
+			};
 			for(int i=0;i<15;i++){
+				HashMap map = new HashMap();
 				try{
-					HashMap map = new HashMap();
 					map.put(""+i+"", ss.getCarry(i).get载具序号()+"-"+ss.getCarry(i).get工位());
 					sszList.add(map);
-					map=null;
-				}catch(Exception e){}
-			}
+				}catch(Exception e){
+					map.put(""+i+"", "");
+					sszList.add(map);
+				};
+				map=null;
+			};
 			result.put("data", mapList);
 			result.put("A_SSX", sszList);
-			ss=null;sszList=null;mapList=null;
+			A=null;sszList=null;mapList=null;ss=null;
 			response.setContentType("text/html;charset=utf-8");
 			response.getWriter().print(result);
 			response.getWriter().close();
@@ -138,27 +167,33 @@ public class PLCAction extends Action{
 			request.setCharacterEncoding("utf-8");
 			response.setCharacterEncoding("utf-8");
 			HashMap map = GetParam.GetParamValue(request, "iso-8859-1", "utf-8");
-//			System.out.println("map="+map);
 			String result=null;
 			Vector<STContent> A=null;
 			//A区
 			if(map.get("gwType").equals("A")){
 				A = PLC.getIntance().STC1;
-			}
 			//B区
-			else{
+			}else if(map.get("gwType").equals("B")){
 				A = PLC.getIntance().STC2;
-			}
+			};
 			//i= 当前第几个工位
 			int i = Integer.parseInt(map.get("gw").toString());
-			//map.get("cm") = 当前工位下的第几个队列  firstST=队列1,secondST=队列2,
-			if(map.get("cm").equals("firstST")){
-				result = A.get(i).firstST.setValueByName(map.get("name").toString(), 
-						map.get("value").toString(), map.get("oldValue").toString());
+			if(map.get("gwType").equals("A")||map.get("gwType").equals("B")){
+				//map.get("cm") = 当前工位下的第几个队列  firstST=队列1,secondST=队列2,
+				if(map.get("cm").equals("firstST")){
+					result = A.get(i).firstST.setValueByName(map.get("name").toString(), 
+							map.get("value").toString(), map.get("oldValue").toString());
+				}else{
+					result = A.get(i).secondST.setValueByName(map.get("name").toString(), 
+							map.get("value").toString(), map.get("oldValue").toString());
+				}
+			//update 选择PLC返回工位
 			}else{
-				result = A.get(i).secondST.setValueByName(map.get("name").toString(), 
-						map.get("value").toString(), map.get("oldValue").toString());
-			}
+				//setValueByName
+				result = PLC.getIntance().setValueByName(map.get("name").toString(), 
+						map.get("value").toString(), map.get("oldValue").toString(), i, 
+						map.get("gwType").equals("C")?1:2);
+			};
 			A=null;i=0;
 			response.setContentType("text/html;charset=utf-8");
 			response.getWriter().print(result);
